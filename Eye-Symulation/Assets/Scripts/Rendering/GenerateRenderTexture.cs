c9ce71fe3857c8edc541a7394c84bba4a409269d
MAIPA01 <marcelipatryk@gmail.com>
2024-11-07 00:49:37 +0100

    EyeVision

--- Eye-Symulation/Assets/Scripts/Rendering/GenerateRenderTexture.cs
+++ Eye-Symulation/Assets/Scripts/Rendering/GenerateRenderTexture.cs
@@ -4,8 +4,22 @@ using UnityEngine.Rendering.Universal;
 
 public class GenerateRenderTexture : ScriptableRendererFeature
 {
+    [System.Serializable]
+    public class PassSettings
+    {
+        public RenderPassEvent renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+        public RenderTexture renderTexture = null;
+    }
+
     class CustomRenderPass : ScriptableRenderPass
     {
+        private PassSettings settings;
+
+        public CustomRenderPass(PassSettings settings) {
+            this.settings = settings;
+            this.renderPassEvent = settings.renderPassEvent;
+        }
+
         // This method is called before executing the render pass.
         // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
         // When empty this render pass will render to the active camera render target.
@@ -13,6 +27,7 @@ public class GenerateRenderTexture : ScriptableRendererFeature
         // The render pipeline will ensure target setup and clearing happens in a performant manner.
         public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
         {
+
         }
 
         // Here you can implement the rendering logic.
@@ -21,6 +36,13 @@ public class GenerateRenderTexture : ScriptableRendererFeature
         // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
+            if (settings.renderTexture != null && renderingData.cameraData.baseCamera != null)
+            {
+                RenderTexture old = renderingData.cameraData.targetTexture;
+                renderingData.cameraData.targetTexture = settings.renderTexture;
+                renderingData.cameraData.baseCamera.Render();
+                renderingData.cameraData.targetTexture = old;
+            }
         }
 
         // Cleanup any allocated resources that were created during the execution of this render pass.
@@ -29,21 +51,22 @@ public class GenerateRenderTexture : ScriptableRendererFeature
         }
     }
 
-    CustomRenderPass m_ScriptablePass;
+    [SerializeField] private PassSettings settings;
+    private CustomRenderPass m_ScriptablePass;
 
     /// <inheritdoc/>
     public override void Create()
     {
-        m_ScriptablePass = new CustomRenderPass();
-
-        // Configures where the render pass should be injected.
-        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
+        m_ScriptablePass = new(settings);
     }
 
     // Here you can inject one or multiple render passes in the renderer.
     // This method is called when setting up the renderer once per-camera.
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
+#if UNITY_EDITOR
+        if (renderingData.cameraData.isSceneViewCamera) return;
+#endif
         renderer.EnqueuePass(m_ScriptablePass);
     }
 }
--- Eye-Symulation/Assets/Shaders/EyeVisionCustomFunctions.hlsl
+++ Eye-Symulation/Assets/Shaders/EyeVisionCustomFunctions.hlsl
@@ -1,33 +1,46 @@
 #ifndef CAMERATESTCUSTOMFUNCTIONS_INCLUDED
 #define CAMERATESTCUSTOMFUNCTIONS_INCLUDED
 
-void RayTracingForEachR_float(float L, float d, float D, float3 P, float3 O, float distantFactor, float angleFactor, UnityTexture2D Texture, out float3 color) {	
+float Remap(float value, float2 InMinMax, float2 OutMinMax)
+{
+    float value01 = (value - InMinMax.x) / (InMinMax.y - InMinMax.x); // value from 0 to 1
+    return value01 * (OutMinMax.y - OutMinMax.x) + OutMinMax.x; // value from outMin to outMax
+}
+
+float2 Remap(float2 value, float2 InMinMax, float2 OutMinMax)
+{
+    return float2(Remap(value.x, InMinMax, OutMinMax), Remap(value.y, InMinMax, OutMinMax));
+}
+
+void RayTracingForEachR_float(float L, float S, float D, float3 P, float3 O, float distantFactor, float angleFactor, float d, UnityTexture2D Texture, out float3 color) {	    
     float pi = 3.14159265f;
-    float invd = 1.f / d;
     float invD = 1.f / D;
     float invL = 1.f / L;
+    //float SSign = S <= 0.f ? -1.f : 1.f;
+    //float d = L / (1.f - L * S * SSign);
+    float invd = 1.f / d;
     
     color = float3(0.f, 0.f, 0.f);
     for (uint distantIndex = 0u; distantIndex < distantFactor; ++distantIndex)
     {
         for (uint angleIndex = 0u; angleIndex < angleFactor; ++angleIndex)
         {
-            float r = float(distantIndex) / float(distantFactor);
-            float fi = (2.f * pi * (float(angleIndex) + r)) / float(angleFactor);
+            float r = float(distantIndex) / distantFactor;
+            float fi = (2.f * pi * (float(angleIndex) + r)) / angleFactor;
         
             float3 R = float3(r * cos(fi), r * sin(fi), 0.f);
 
 			float3 RO = O - R;
             float3 PR = R - P;
-			float3 RB = ((D + d) * RO) * invd + (D * PR) * invL;
+            float3 RB = ((D - d) * RO) * invd + (D * PR) * invL;
 
 			float3 B = RB + R;
             float2 uv = -B.xy;
-			uv *= L * invD;
+            uv *= L * invD;
+            uv = Remap(uv, float2(-1.f, 1.f), float2(0.f, 1.f));
             uv = clamp(uv, 0.f, float2((Texture.texelSize.z - 1.f) / Texture.texelSize.z, (Texture.texelSize.w - 1.f) / Texture.texelSize.w)); // From 0 to 1 in all directions
 
 			color += SAMPLE_TEXTURE2D_LOD(Texture.tex, Texture.samplerstate, uv.xy, 0).xyz;
-            //color += float3(uv.xy, B.z);
         }
 	}
     color /= distantFactor * angleFactor;
